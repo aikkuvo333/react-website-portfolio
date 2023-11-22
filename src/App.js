f8ea4a7a0b307e8781e2548c4d6ef740835399e1
aivanvo <aivan.vo@metropolia.fi>
2023-11-23 01:03:37 +0200

    All of the pages added to the App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -9,11 +12,13 @@ function App() {
     <Router>
      <NavBar/>
      <Routes>
-     <Route path='/' exact />
+     <Route path='/' exact Component={Home}/>
+     <Route path='/projects' Component={Projects}/>
+     <Route path='/contact' exact Component={Contact}/>
      </Routes>
     </Router>
     </>
-  );
+  )
 }
 
 export default App;
