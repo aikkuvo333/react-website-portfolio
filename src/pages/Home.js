790e2d1fabce7f6e27924d0fc9e530f5642e10cc
aivanvo <aivan.vo@metropolia.fi>
2023-11-23 01:02:47 +0200

    MainSection and Cards added to the home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App';
+import MainSection from '../components/MainSection';
+import Cards from '../components/Cards';
+
+function Home() {
+  return (
+    <>
+      <MainSection></MainSection>
+      <Cards></Cards>
+    </>
+  );
+}
+
+export default Home;
