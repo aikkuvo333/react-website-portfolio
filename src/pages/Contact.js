359110b3a98c5174f349ab2efcda84cba94e5cdf
aivanvo <aivan.vo@metropolia.fi>
2023-11-23 01:03:19 +0200

    Placeholder texts for Contact and Projects -pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.js';
+
+export default function Contact() {
+  return (
+    <h1 className='contact'>
+      Contact me from here 
+    </h1>
+  )
+}
---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '/Users/aivanvo/DX project/react-website-portfolio/src/App.js';
+
+export default function Projects() {
+  return (
+    <h1 className='projects'>
+      My projects 
+    </h1>
+  )
+}
