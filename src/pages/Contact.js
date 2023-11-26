30233f3ff2082e9a0f11a6d3c923691850951a98
aivanvo <aivan.vo@metropolia.fi>
2023-11-26 23:43:56 +0200

    Contact Form added to the Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import '../App.js';
+import ContactForm from '../components/ContactForm.js';
+import './Contact.css';
+import { Link } from 'react-router-dom';
 
 export default function Contact() {
   return (
-    <h1 className='contact'>
-      Contact me from here 
-    </h1>
+    <div>
+      <div>
+        <h1>Contact</h1>
+        <p>Want to work together on a project?</p>
+      </div>
+      <div>
+        <ContactForm></ContactForm>
+      </div>
+      <div>
+        <h2>Connect elsewhere!</h2>
+        <div>
+          <ul>
+            <li>
+              <Link>aivan.vo@metropolia.fi</Link>
+            </li>
+            <li>
+              <Link>LinkedIn</Link>
+            </li>
+            <li>
+              <Link>Github</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
