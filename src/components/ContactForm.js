7bceb4b131ea96d88805b0003dff1e951a7d9e4e
aivanvo <aivan.vo@metropolia.fi>
2023-12-12 16:12:30 +0200

    Send email using email js and empty the fields after succesfull submit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,66 +1,45 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
-const ContactForm = () => {
+export const ContactForm = () => {
+  const form = useRef();
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
+    from_name: '',
+    from_email: '',
     message: '',
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3uatax8', 'template_t94dikc', form.current, 'RtDBbwf84_FZuK31o')
+      .then((result) => {
+          console.log(result.text);
+
+          setFormData({
+            from_name: '',
+            from_email: '',
+            message: '',
+          });
+      }, (error) => {
+          console.log(error.text);
+      });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Send the submission to me
-    console.log('Form submitted:', formData);
-    setFormData({
-      name: '',
-      email: '',
-      message: '',
-    });
+  const handleChange = (e) => {
+    // Update form data as the user types
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        ></textarea>
-      </div>
-      <button type="submit">Submit</button>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" value={formData.from_name} onChange={handleChange} />
+      <label>Email</label>
+      <input type="email" name="from_email" value={formData.from_email} onChange={handleChange}/>
+      <label>Message</label>
+      <textarea name="message" value={formData.message} onChange={handleChange}/>
+      <input type="submit" value="Send" />
     </form>
   );
 };
