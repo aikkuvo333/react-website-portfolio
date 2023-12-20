1c461035805f72f8d5d8ca18e3237ac6964ed4e6
aivanvo <aivan.vo@metropolia.fi>
2023-12-21 00:28:37 +0200

    Icons downloaded and added for some

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../App.js';
 import ContactForm from '../components/ContactForm.js';
 import './Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Contact() {
   return (
@@ -18,10 +20,14 @@ export default function Contact() {
         <div>
           <ul>
             <li>
-              <a href='https://www.linkedin.com/in/aivan-vo/'>LinkedIn</a>
+              <a href='https://www.linkedin.com/in/aivan-vo/' target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+              </a>
             </li>
             <li>
-              <a href='https://www.github.com/aikkuvo333/'>Github</a>
+              <a href='https://www.github.com/aikkuvo333/' target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+              </a>
             </li>
           </ul>
         </div>
