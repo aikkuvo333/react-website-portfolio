20184c264fb711deb37ebbacdef2e49a39d5b542
aivanvo <aivan.vo@metropolia.fi>
2023-12-22 13:24:12 +0200

    Project descriptions added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,6 +6,7 @@ function Cards() {
   return (
     <div className='cards' id='projects_section'>
       <h1>Projects</h1>
+      <p>Here you can find some of my personal as well as university projects</p>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
@@ -30,44 +31,58 @@ function Cards() {
             <CardItem src='/images/image2.jpg'
               title='Chess game application'
               label='java'
-              details=''
+              details='Project Scope:
+              Developing a local web-based chess app in Java using Maven and integrating MariaDB for efficient data storage.
+              Tasks:
+              - Participated in UI/UX design and application logic.
+              - Served as one of the Scrum Masters, ensuring agile efficiency. - Conducted testing and comprehensive documentation.
+              Outcome:
+              User-friendly and accessible local web-based chess application with multi-language options.'
               link=''
-              linkTitle=''
-              skills='' />
-          </ul>
-          <ul className='cards__items'>
+              linkTitle='Github'
+              skills='java mariadb'/>
             <CardItem src='/images/image2.jpg'
               title='Pharmacy capacity simulator'
               label='java'
-              details=''
-              link=''
-              linkTitle=''
-              skills='' />
+              details='Project Scope:
+              Creating a pharmacy capacity testing simulator using Java, CSS, Hibernate, and MariaDBConnector.
+              Tasks:
+              -Implemented application logic
+              -Conducted thorough testing and documentation
+              Outcome:
+              Capacity testing simulator'
+              link='https://github.com/aikkuvo333/PharmacySimulator'
+              linkTitle='Github'
+              skills='Java, CSS, Hibernate, MariaDBConnector' />
             <CardItem
               src='images/image2.jpg'
               title='Website Behaviour Tracking'
               label='google analytics'
-              details=''
+              details='Project Scope:
+              Analyzing and reporting User behaviour for a website to increase the conversion rate.
+              Tasks:
+              -Implemented Google Tag Manager for an construction engineering company’s website -Using Hotjar web analytics tool for visual representations of user interaction heatmaps -Improvement suggestions for the client based on the analytics
+              -Created a customized Google Studios report for a Google Ad campaign
+              -Provided documentation for report and Tag Manager
+              Outcome:
+              After the recommendations were implemented the conversion rate increased by 65% percent. (from 6 to 10 conversions per month)'
               link=''
               linkTitle=''
-              skills='' />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem src='/images/image2.jpg'
+              skills='Google analytics, Hotjar' />
+            <CardItem src='/images/track.png'
               title='Track Calorie tracker'
               label='vue'
-              details=''
-              link=''
-              linkTitle=''
-              skills='' />
-            <CardItem
-              src='images/image2.jpg'
-              title='Project'
-              label=''
-              details=''
-              link=''
-              linkTitle=''
-              skills='' />
+              details='Project Scope:
+              Creating a Vue-based calorie tracking Web-application using JavaScript, HTML, and CSS.
+              Tasks:
+              -Designed UI for optimal user experience.
+              -Integrated APIs for recipe retrieval.
+              -Developed application logic for precise calorie tracking.
+              Outcome:
+              An intuitive calorie tracking Web-application with multiple features such as recipe search and training video accessibility.'
+              link='https://github.com/aikkuvo333/TrackWebApplication'
+              linkTitle='Github'
+              skills='Vue, JavaScript, HTML, CSS, API' />
           </ul>
         </div>
       </div>
