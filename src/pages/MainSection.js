d635dc1c336b97030d66eb335256c78b8602754a
aivanvo <aivan.vo@metropolia.fi>
2024-01-31 13:50:28 +0200

    main container text edits

--- src/pages/MainSection.js
+++ src/pages/MainSection.js
@@ -7,7 +7,10 @@ function MainSection() {
   return (
     <div className='main-container'>
       <h1>AIVAN VO</h1>
-      <p>Hello! Welcome to my portfolio</p>
+      <p>Hello! Welcome to my portfolio. 
+        I am a student of Information and Communication Technology, specializing in software production. 
+        I am an enthusiastic, solution-oriented learner with experience in various industry projects. 
+        I have particular expertise in creating automated tests, automation testing, as well as JavaScript, Java, and SQL.</p>
       <div className='main-btns'>
         <ContactBtn className='btns' btnStyle='btn--outline' btnSize='btn--large'>
           CONTACT ME
