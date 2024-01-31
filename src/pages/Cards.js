d10ddf6b064ab9e5659f986b3889ba3d205fe602
aivanvo <aivan.vo@metropolia.fi>
2024-01-31 19:14:03 +0200

    separator added for pages

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -5,14 +5,17 @@ import './Cards.css';
 function Cards() {
   return (
     <div className='cards' id='projects_section'>
-      <h1>Projects</h1>
-      <p>Here you can find some of my personal as well as university projects</p>
+      <div className='cards__projects'>
+        <h1>Projects</h1>
+        <div className='cards__separator'></div>
+        <p>Here you can find some of my personal as well as university projects</p>
+      </div>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem src='/images/ecolounge.jpg'
               title='Ecolounge'
-              description='Ecolounge is sun chair made out of recycled ocean plastic waste for the marina of Valencia.'
+              description='Sun chair made out of recycled ocean plastic waste for the marina of Valencia.'
               label='product design'
               details='
                 Project Scope: 
@@ -31,7 +34,7 @@ function Cards() {
               skills='Product design' />
             <CardItem src='/images/shakki.png'
               title='Chess game'
-              description='Chess game is a local web-based chess app'
+              description='Local web-based chess app designed for players seeking precise move tracking'
               label='java, maven, mariadb'
               details='Project Scope:
               Developing a local web-based chess app in Java using Maven and integrating MariaDB for efficient data storage.
@@ -40,12 +43,12 @@ function Cards() {
               - Served as one of the Scrum Masters, ensuring agile efficiency. - Conducted testing and comprehensive documentation.
               Outcome:
               User-friendly and accessible local web-based chess application with multi-language options.'
-              link=''
-              linkTitle=''
+              link='https://github.com/aikkuvo333/Chess-game'
+              linkTitle='Github'
               skills='Java, MariaDB' />
             <CardItem src='/images/apteekki.png'
               title='Pharmacy simulator'
-              description='Pharmacy capacity simulator a testing simulator'
+              description='Pharmacy capacity testing simulator to optimize the handling of visitor traffic in pharmacies.'
               label='java'
               details='Project Scope:
               Creating a pharmacy capacity testing simulator using Java, CSS, Hibernate, and MariaDBConnector.
@@ -60,7 +63,7 @@ function Cards() {
             <CardItem
               src='images/analytics.png'
               title='Website Behaviour Tracking'
-              description='Analyzing and reporting User behaviour for a construction engineering company’s website'
+              description='Analyzing and reporting user behaviour for a website to increase the conversion rate.'
               label='google analytics'
               details='Project Scope:
               Analyzing and reporting User behaviour for a website to increase the conversion rate.
@@ -75,7 +78,7 @@ function Cards() {
               skills='Google analytics, Hotjar, Freelance' />
             <CardItem src='/images/track.png'
               title='Track'
-              description='Track is a calorie tracking Web-application'
+              description='Calorie Tracking Web Application featuring built-in recipes and training videos.'
               label='vue, javascript, html, css'
               details='Project Scope:
               Creating a Vue-based calorie tracking Web-application using JavaScript, HTML, and CSS.
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../App.js';
 import ContactForm from '../components/ContactForm.js';
 import './Contact.css';
-
+import './Cards.css';
 
 export default function Contact() {
   return (
     <div id='contact_section'>
-      <div className='contact-text-container'>
+      <div className='contact-text-container cards__projects'>
         <h1>Contact</h1>
+        <div className='cards__separator'></div>
         <p>Want to work together on a project?</p>
       </div>
         <ContactForm/>
