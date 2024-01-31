e350457b02e3dd97800c87cbd28440997b262514
aivanvo <aivan.vo@metropolia.fi>
2024-01-31 20:35:15 +0200

    Remove Website if there is none from the lightbox

--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -1,32 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Lightbox.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleChevronLeft, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faCircleChevronLeft, faArrowUpRightFromSquare, faEarthEurope } from '@fortawesome/free-solid-svg-icons';
 
 function Lightbox(props) {
+  const [existingAttribute, setExistingAttribute] = useState(false);
 
   const closeLightbox = () => {
     props.onClick();
     document.body.classList.remove('lightbox-open');
-
   };
 
   useEffect(() => {
+    // Check if the attribute exists in props
+    if (props.link) {
+      setExistingAttribute(true);
+    }
     document.body.classList.add('lightbox-open');
+    document.querySelector('aside').classList.add('appear');
     return () => {
       document.body.classList.remove('lightbox-open');
     };
-  }, []);
+  }, [props.link]);
 
   return (
 
     <div>
       <div className="backdrop" onClick={closeLightbox} />
-      <aside>
+      <aside className='aside'>
         <div className="lightbox">
           <div className='lightbox-top'>
-            <FontAwesomeIcon icon={faCircleChevronLeft} className="return" onClick={closeLightbox}/>
-            <div className="separator"/>          
+            <FontAwesomeIcon icon={faCircleChevronLeft} className="return" onClick={closeLightbox} />
+            <div className="separator" />
           </div>
           <div className="lightbox-content">
             <h2>{props.title}</h2>
@@ -44,16 +49,16 @@ function Lightbox(props) {
                   (<div key={index} className='skills_skill'>{skills}</div>))}
               </div>
             </div>
-            <div className='projectDetails_links'>
-              <h4 className='projectDetails_contentTitle'>Website</h4>
+            {existingAttribute && <div className='projectDetails_links'>
+              <h4 className='projectDetails_contentTitle'> <FontAwesomeIcon icon={faEarthEurope} /> Website</h4>
               <a href={props.link} target="_blank" rel="noreferrer" aria-label='open website link'>{props.linkTitle}</a>
-            </div>
+            </div>}
           </div>
         </div>
-        <a href={props.link} className='projectDetails_openProject' target='_blank' rel='noreferrer' aria-label='open website link'>
+        {existingAttribute && <a href={props.link} className='projectDetails_openProject' target='_blank' rel='noreferrer' aria-label='open website link'>
           Open project
-          <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='openLink'/>
-        </a>
+          <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='openLink' />
+        </a>}
       </aside>
     </div>
   );
