bac1aac3f69aee37d20e623fe1840e100f086baf
aivanvo <aivan.vo@metropolia.fi>
2024-02-05 17:38:18 +0200

    Testing if the changes will be visible in amplify

--- src/pages/pages_js/MainSection.js
+++ src/pages/pages_js/MainSection.js
@@ -6,7 +6,7 @@ import '../pages_css/MainSection.css';
 function MainSection() {
   return (
     <div className='main-container'>
-      <h1>AIVAN VO</h1>
+      <h1>AIVeferfrewAN VO</h1>
       <p>Hello! Welcome to my portfolio. 
         I'm a final year student of Information and Communication Technology, 
         specializing in <b>Software Engineering</b>. 
